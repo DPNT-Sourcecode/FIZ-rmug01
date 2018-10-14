02f09132c1546875f13e6bd7a62e860c877731da
sbx_user1060 <sbx_user1060@ip-10-129-165-13.eu-west-2.compute.internal>
2018-10-14 17:54:17 +0000

    Sun Oct 14 17:54:17 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -26,7 +26,7 @@ public class FizzBuzzSolution {
     		} else {
     			deluxeResponse = "deluxe";
     		}
-    		
+    		response = deluxeResponse;
     		deluxeTrue = true;
     	}
     	
@@ -62,8 +62,8 @@ public class FizzBuzzSolution {
     }
     
     public static void main (String[] args) {
-    	//11 22 1111
-    	String response = fizzBuzz(555);
+    	//22 4444 11
+    	String response = fizzBuzz(22);
     	System.out.println(response);
     }
 

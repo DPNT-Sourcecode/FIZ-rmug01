35b30a6bae71945690af6d3a393a4f87318b68fc
sbx_user1060 <sbx_user1060@ip-10-129-165-13.eu-west-2.compute.internal>
2018-10-14 17:09:55 +0000

    Sun Oct 14 17:09:55 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -9,16 +9,20 @@ public class FizzBuzzSolution {
     		return "Illegal Input";
     	}
     	String response = number.toString();
+    	boolean fizzTrue = false;
+    	boolean buzzTrue = false;
     	
     	if(number%5 == 0 || Integer.valueOf(number).toString().indexOf("5") != -1) {
     		response = "buzz";
+    		buzzTrue = true;
     	}
     	
     	if(number%3 == 0 || Integer.valueOf(number).toString().indexOf("3") != -1) {
     		response = "fizz";
+    		fizzTrue = true;
     	}
     	
-    	if(number%5 == 0 && number%3 == 0) {
+    	if(fizzTrue && buzzTrue) {
     		response = "fizz buzz";
     	}
     	

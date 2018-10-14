cda40d9210c527470a0c5c8d10f274b6f3f08ec6
sbx_user1060 <sbx_user1060@ip-10-129-165-13.eu-west-2.compute.internal>
2018-10-14 18:03:12 +0000

    Sun Oct 14 18:03:12 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -17,45 +17,53 @@ public class FizzBuzzSolution {
     	boolean fizzTrue = false;
     	boolean buzzTrue = false;
     	boolean deluxeTrue = false;
+    	boolean fakeDeluxeTrue = false;
     	String pattern = "^([0-9])\\1*$";//str.matches("([0-9]){6}")
     	Pattern p = Pattern.compile(pattern);
     	Matcher m = p.matcher(number.toString());
     	if(number>10 && m.matches()) {
     		if(number%2!=0) {
     			deluxeResponse = "fake deluxe";
+    			fakeDeluxeTrue = true;
     		} else {
     			deluxeResponse = "deluxe";
+    			deluxeTrue = true;
     		}
     		response = deluxeResponse;
-    		deluxeTrue = true;
+    		
     	}
     	
     	
     	
     	if(number%5 == 0 || Integer.valueOf(number).toString().indexOf("5") != -1) {
     		response = "buzz";
-    		buzzTrue = true;
-    		if(deluxeTrue) {
-    			response = "buzz "+deluxeResponse;
+    		if(fakeDeluxeTrue) {
+    			deluxeResponse = "fake deluxe";
+    		} else {
+    			deluxeResponse = "deluxe";
     		}
+    		
+    		buzzTrue = true;
+    		/*if(deluxeTrue) {
+    			response = "buzz "+deluxeResponse;
+    		}*/
     	}
     	
     	if(number%3 == 0 || Integer.valueOf(number).toString().indexOf("3") != -1) {
     		response = "fizz";
-    		fizzTrue = true;
-    		if(deluxeTrue) {
-    			response = "fizz "+deluxeResponse;
+    		if(fakeDeluxeTrue) {
+    			deluxeResponse = "fake deluxe";
+    		} else {
+    			deluxeResponse = "deluxe";
     		}
+    		fizzTrue = true;
+    		/*if(deluxeTrue) {
+    			response = "fizz "+deluxeResponse;
+    		}*/
     	}
     	
-    	//if(number>10 && number.)
-    	
     	if(fizzTrue && buzzTrue) {
-    		if(deluxeTrue) {
-    			response = "fizz buzz "+deluxeResponse;
-    		} else {
-    			response = "fizz buzz";
-    		}
+    		response = "fizz buzz ";    		
     	}
     	
         return response;
